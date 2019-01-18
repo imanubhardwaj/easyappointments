e3661305cf45d048b13b8e0b913cc314027915f2
Manu <mnbhardwaj888@gmail.com>
2019-01-18 11:56:30 +0530

    renaming

--- src/application/views/user/logout.php
+++ src/application/views/user/logout.php
@@ -65,14 +65,14 @@
 
         <br>
 
-        <a href="<?= site_url() ?>" class="btn btn-success btn-large">
+        <a href="<?= site_url() ?>" class="btn btn-primary btn-large">
             <span class="glyphicon glyphicon-calendar"></span>
             <?= lang('book_appointment_title') ?>
         </a>
 
         <a href="<?= site_url('backend') ?>" class="btn btn-default btn-large">
             <span class="glyphicon glyphicon-wrench"></span>
-            <?= lang('backend_section') ?>
+            <?= lang('dashboard') ?>
         </a>
     </div>
 </body>
