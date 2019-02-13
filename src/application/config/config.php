ea8c37a66ca05de684a7f90ad1370c5a32faa0e0
Manu <mnbhardwaj888@gmail.com>
2019-02-13 23:27:32 +0530

    session expiration time to 2 weeks

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -306,7 +306,7 @@ $config['encryption_key'] = Config::BASE_URL;
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ea_session';
-$config['sess_expiration'] = 7200;
+$config['sess_expiration'] = 1209600;
 $config['sess_save_path'] = __DIR__ . '/../../storage/sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
